8c2c25fb90b079ba0c74895133971488208f91d7
Evgeniy Ivon <ivoneug@gmail.com>
2018-03-01 11:23:16 +0300

    fixed navigation title component for android

--- src/components/common/NavigationTitle.js
+++ src/components/common/NavigationTitle.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Platform } from 'react-native';
 
 const NavigationTitle = (props) => {
     const { titleText } = props;
-    const { containerStyle, textStyle } = styles;
+    const { containerStyle, textStyleIOS, textStyleAndroid } = styles;
+
+    const textStyle = Platform.OS === 'android' ? textStyleAndroid : textStyleIOS;
 
     return (
         <View style={containerStyle}>
@@ -17,12 +19,20 @@ const styles = {
     containerStyle: {
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginLeft: 10,
+        marginRight: 10
     },
-    textStyle: {
+    textStyleIOS: {
         fontSize: 17,
         fontWeight: '600',
         marginLeft: 10
+    },
+    textStyleAndroid: {
+        fontSize: 17,
+        fontWeight: '400',
+        marginLeft: 10,
+        color: 'black'
     }
 };
 
