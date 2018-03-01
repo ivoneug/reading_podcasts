6efa004aaf5be8b9bfbbf7d1942da4fee777926f
Evgeniy Ivon <ivoneug@gmail.com>
2018-03-01 20:47:35 +0300

    changed icon for android force fullscreen mode for the app added localization for app name

--- android/app/src/main/java/ru/bibobo/reading_podcasts/MainApplication.java
+++ android/app/src/main/java/ru/bibobo/reading_podcasts/MainApplication.java
@@ -3,6 +3,7 @@ package ru.bibobo.reading_podcasts;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.tanguyantoine.react.MusicControl;
 import com.zmxv.RNSound.RNSoundPackage;
 import fm.indiecast.rnaudiostreamer.RNAudioStreamerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeLocalizationPackage(),
             new MusicControl(),
             new RNSoundPackage(),
             new RNAudioStreamerPackage(),
