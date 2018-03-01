ada7d818bcbb6aab773978e611cfd556703fefd2
Evgeniy Ivon <ivoneug@gmail.com>
2018-03-01 11:22:45 +0300

    implemented animatable version of spinner indicator

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,20 +1,53 @@
-import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
-const Spinner = ({ size }) => {
-    return (
-        <View style={styles.spinnerStyle}>
-            <ActivityIndicator size={size || 'large'} />
-        </View>
-    );
-};
+class Spinner extends Component {
+    state = { visible: true };
+
+    componentWillReceiveProps(nextProps) {
+        const { visible = true } = nextProps;
+
+        if (!visible) {
+            this.view.fadeOut(300).then(() => {
+                this.setState({ visible: false });
+            });
+        }
+    }
+
+    render() {
+        const { visible } = this.state;
+        const { size } = this.props;
+        const { spinnerStyle, hideStyle } = styles;
+
+        const style = visible ? spinnerStyle : [spinnerStyle, hideStyle];
+
+        return (
+            <Animatable.View
+                animation='fadeIn'
+                style={style}
+                ref={(view) => { this.view = view; }}
+            >
+                {visible ? <ActivityIndicator size={size || 'large'} /> : null}
+            </Animatable.View>
+        );
+    }
+}
 
 const styles = {
+    hideStyle: {
+        width: 0,
+        height: 0
+    },
     spinnerStyle: {
-        flex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
         alignItems: 'center',
         justifyContent: 'center'
     }
 };
 
-export { Spinner };
+export default Spinner;
